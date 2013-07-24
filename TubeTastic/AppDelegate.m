a590e871b28b76d46fe32bc5598b387cb404657c
Rick Osborne <rosborne@fullsail.com>
2013-07-24 14:29:28 -0400

    Show Splash view controller

--- TubeTastic/AppDelegate.m
+++ TubeTastic/AppDelegate.m
@@ -15,7 +15,9 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
 //    self.window.backgroundColor = [UIColor whiteColor];
-//    [self.window makeKeyAndVisible];
+    self.splashViewController = [[SplashViewController alloc] init];
+    self.window.rootViewController = self.splashViewController;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
