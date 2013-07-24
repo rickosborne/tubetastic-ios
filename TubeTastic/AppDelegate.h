75bd721912f0fd60d69c512863633269eeddb281
Rick Osborne <rosborne@fullsail.com>
2013-07-24 10:28:24 -0400

    Initial Commit

---
+++ TubeTastic/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TubeTastic
+//
+//  Created by Rick Osborne on 7/24/13.
+//  Copyright (c) 2013 rick osborne dot org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
