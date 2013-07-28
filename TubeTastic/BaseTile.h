59df17ebdeb64893cd8b0cdbabf9c50e54ddf1f3
Rick Osborne <rosborne@fullsail.com>
2013-07-28 01:44:30 -0400

    Get rid of default template comments

--- TubeTastic/BaseTile.h
+++ TubeTastic/BaseTile.h
@@ -2,8 +2,6 @@
 // Created by Rick Osborne on 7/25/13.
 // Copyright (c) 2013 rick osborne dot org. All rights reserved.
 //
-// To change the template use AppCode | Preferences | File Templates.
-//
 
 
 #import <Foundation/Foundation.h>
@@ -22,20 +20,12 @@
 
 @end
 
-//static const NSString* DIRECTION_NORTH = @"N";
-//static const NSString* DIRECTION_SOUTH = @"S";
-//static const NSString* DIRECTION_EAST  = @"E";
-//static const NSString* DIRECTION_WEST  = @"W";
 static const int DEGREES_NORTH =   0;
 static const int DEGREES_EAST  =  90;
 static const int DEGREES_SOUTH = 180;
 static const int DEGREES_WEST  = 270;
 static const int directionCount = 4;
 static const int outletDegrees[] = {DEGREES_NORTH, DEGREES_EAST, DEGREES_SOUTH, DEGREES_WEST};
-//static const NSString* outletDirections[] = { @"N", @"E", @"S", @"W" };
-//static const NSDictionary* directionFromDegrees;
-//static const NSDictionary* outletOffsets;
-//static const NSDictionary* outletRotationsReverse;
 
 @interface BaseTile : NSObject
 {
--- TubeTastic/GamePrefs.m
+++ TubeTastic/GamePrefs.m
@@ -2,8 +2,6 @@
 // Created by Rick Osborne on 7/27/13.
 // Copyright (c) 2013 rick osborne dot org. All rights reserved.
 //
-// To change the template use AppCode | Preferences | File Templates.
-//
 
 
 #import "GamePrefs.h"
